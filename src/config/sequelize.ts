30eaf163d9fb1c4c632127debe1a974eff665618
zdebs <zdebski1@gmail.com>
2025-04-28 22:29:59 -0400

    adding typescript

---
+++ src/config/sequelize.ts
@@ -0,0 +1,14 @@
+require("dotenv").config();
+import { Sequelize } from "sequelize";
+
+const sequelizeDb = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+    }
+  );
+  
+export default sequelizeDb;
